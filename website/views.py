a9680b1111243788d594d6b5ea3016f224a66c4e
khushi <khushi@gmail.com>
2024-02-08 20:13:59 +0530

    'registeration'

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages 
+from .forms import SignUpForm 
 
 # Create your views here.
 
@@ -18,3 +19,26 @@ def home(request):
             return redirect('home')
     else:    
         return render(request,'home.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out...") 
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,'You have Successfully Registered! Welcome!')
+            return redirect('home')
+    else:
+        form= SignUpForm()
+        return render(request,'register.html',{'form':form})    
+    return render(request,'register.html',{'form':form})
+
+   
